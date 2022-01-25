23670b93e330c9f95a642a984a88a162a40ad1a8
vuongle2609 <vuongle2609@gmail.com>
2022-01-25 21:56:02 +0700

    add choose genres section

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import store from "../state";
 
 function ModalSearch() {
@@ -15,7 +16,7 @@ function ModalSearch() {
     "
     >
       <div className="w-11/12">
-        <h2 className="dark:text-white font-medium text-2xl mb-4">Search</h2>
+        <h2 className="dark:text-white font-medium text-2xl mb-4">Tìm kiếm</h2>
       </div>
       <input
         type="text"
@@ -27,9 +28,256 @@ function ModalSearch() {
 }
 
 function ModalGenres() {
+  const { setGenres, setCanvas } = store();
+  const genresList = [
+    {
+      genre: "Action",
+      EP: "action",
+    },
+    {
+      genre: "Adult",
+      EP: "adult",
+    },
+    {
+      genre: "Adventure",
+      EP: "adventure",
+    },
+    {
+      genre: "Anime",
+      EP: "anime",
+    },
+    {
+      genre: "Chuyển Sinh",
+      EP: "chuyen-sinh",
+    },
+    {
+      genre: "Cổ Đại",
+      EP: "co-dai",
+    },
+    {
+      genre: "Comedy",
+      EP: "comedy",
+    },
+    {
+      genre: "Comic",
+      EP: "comic",
+    },
+    {
+      genre: "Demons",
+      EP: "demons",
+    },
+    {
+      genre: "Detective",
+      EP: "detective",
+    },
+    {
+      genre: "Doujinshi",
+      EP: "doujinshi",
+    },
+    {
+      genre: "Drama",
+      EP: "drama",
+    },
+    {
+      genre: "Đam Mỹ",
+      EP: "dam-my",
+    },
+    {
+      genre: "Ecchi",
+      EP: "ecchi",
+    },
+    {
+      genre: "Fantasy",
+      EP: "fantasy",
+    },
+    {
+      genre: "Gender Bender",
+      EP: "gender-bender",
+    },
+    {
+      genre: "Harem",
+      EP: "harem",
+    },
+    {
+      genre: "Historical",
+      EP: "historical",
+    },
+    {
+      genre: "Horror",
+      EP: "horror",
+    },
+    {
+      genre: "Huyền Huyễn",
+      EP: "huyen-huyen",
+    },
+    {
+      genre: "Isekai",
+      EP: "isekai",
+    },
+    {
+      genre: "Josei",
+      EP: "josei",
+    },
+    {
+      genre: "Mafia",
+      EP: "mafia",
+    },
+    {
+      genre: "Magic",
+      EP: "magic",
+    },
+    {
+      genre: "Manhua",
+      EP: "manhua",
+    },
+    {
+      genre: "Manhwa",
+      EP: "manhwa",
+    },
+    {
+      genre: "Martial Arts",
+      EP: "martial-arts",
+    },
+    {
+      genre: "Mature",
+      EP: "mature",
+    },
+    {
+      genre: "Military",
+      EP: "military",
+    },
+    {
+      genre: "Mystery",
+      EP: "mystery",
+    },
+    {
+      genre: "Ngôn Tình",
+      EP: "ngon-tinh",
+    },
+    {
+      genre: "One shot",
+      EP: "one-shot",
+    },
+    {
+      genre: "Psychological",
+      EP: "psychological",
+    },
+    {
+      genre: "Romance",
+      EP: "romance",
+    },
+    {
+      genre: "School Life",
+      EP: "school-life",
+    },
+    {
+      genre: "Sci-fi",
+      EP: "sci-fi",
+    },
+    {
+      genre: "Seinen",
+      EP: "seinen",
+    },
+    {
+      genre: "Shoujo",
+      EP: "shoujo",
+    },
+    {
+      genre: "Shoujo Ai",
+      EP: "shoujo-ai",
+    },
+    {
+      genre: "Shounen",
+      EP: "shounen",
+    },
+    {
+      genre: "Shounen Ai",
+      EP: "shounen-ai",
+    },
+    {
+      genre: "Slice of life",
+      EP: "slice-of-life",
+    },
+    {
+      genre: "Smut",
+      EP: "smut",
+    },
+    {
+      genre: "Sports",
+      EP: "sports",
+    },
+    {
+      genre: "Supernatural",
+      EP: "supernatural",
+    },
+    {
+      genre: "Tragedy",
+      EP: "tragedy",
+    },
+    {
+      genre: "Trọng Sinh",
+      EP: "trong-sinh",
+    },
+    {
+      genre: "Truyện Màu",
+      EP: "truyen-mau",
+    },
+    {
+      genre: "Webtoon",
+      EP: "webtoon",
+    },
+    {
+      genre: "Xuyên Không",
+      EP: "xuyen-khong",
+    },
+    {
+      genre: "Yaoi",
+      EP: "yaoi",
+    },
+    {
+      genre: "Yuri",
+      EP: "yuri",
+    },
+    {
+      genre: "Mecha",
+      EP: "mecha",
+    },
+    {
+      genre: "Cooking",
+      EP: "cooking",
+    },
+    {
+      genre: "Trùng Sinh",
+      EP: "trung-sinh",
+    },
+    {
+      genre: "Gourmet",
+      EP: "gourmet",
+    },
+  ];
+
   return (
-    <p>jdhfjksdhf</p>
-  )
+    <div className="bg-white dark:bg-bdark h-fit w-10/12 self-center py-5 px-3 rounded-sm">
+      <div className="w-11/12">
+        <h2 className="dark:text-white font-medium text-2xl mb-4">Thể loại</h2>
+      </div>
+      <div className=" w-full h-[400px] rounded-sm overflow-scroll overflow-x-hidden scrollbar-cus row">
+        {genresList.map((genre, index) => (
+          <Link
+            key={index}
+            to={`/list?genre=${genre.EP}`}
+            onClick={() => {
+              setGenres();
+              setCanvas();
+            }}
+            className="h-6 flex items-center px-3 col c-6 font-medium active:text-primary active:dark:text-primary select-none hover:cursor-pointer overflow-hidden whitespace-nowrap text-ellipsis dark:text-white"
+          >
+            {genre.genre}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default function Modal() {
