ff499ae1e481b96d04c7502d868034eaf0f8fa8d
vuongle2609 <vuongle2609@gmail.com>
2022-04-13 02:36:10 +0700

    fix bugs

--- src/components/OffCanvas.js
+++ src/components/OffCanvas.js
@@ -87,7 +87,8 @@ function UserSections(props) {
   } else {
     render = (
       <Link
-        to={`/user?name=${userData.username}`}
+        to={`/user`}
+        onClick={() => setCanvas(false)}
         className="w-full dark:bg-sdarks bg-slights rounded-xl flex items-center justify-start p-5"
       >
         <div
